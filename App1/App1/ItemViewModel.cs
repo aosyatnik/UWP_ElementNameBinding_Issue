bea9e40cab9092ddd94db34814b74772f18180fc
Anna Melashkina <melashkina@outlook.com>
2020-03-02 15:19:52 +0100

    first commit

---
+++ App1/App1/ItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace App1
+{
+    public class ItemViewModel : ViewModelBase
+    {
+        public static int Index = 0;
+        public string Name { get; set; }
+
+        public ItemViewModel()
+        {
+            Index++;
+            Name = $"{Index}";
+        }
+    }
+}
---
+++ App1/App1/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+
+namespace App1
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<ItemViewModel> Items { get; set; } = new ObservableCollection<ItemViewModel>();
+
+        public bool IsStaggeringEnabled { get; set; } = true;
+
+        public MainViewModel()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Items.Add(new ItemViewModel());
+            }
+        }
+    }
+}
